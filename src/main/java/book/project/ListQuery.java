76864524ab463dde22e4d3373f29bdeaa92ed3ca
Alexandr Medvedev <medweather@mail.ru>
2019-07-18 10:07:04 +0300

    optimization project

--- src/main/java/book/project/ListQuery.java
+++ src/main/java/book/project/ListQuery.java
@@ -2,10 +2,7 @@ package book.project;
 
 import book.project.model.Book;
 import book.project.model.User;
-import org.hibernate.Criteria;
 import org.hibernate.Session;
-import org.hibernate.criterion.Order;
-import org.hibernate.criterion.Projections;
 
 import javax.enterprise.context.RequestScoped;
 import java.util.ArrayList;
@@ -30,33 +27,5 @@ public class ListQuery {
         Session session = HibernateUtil.getSessionFactory().openSession();
         books = session.createCriteria(Book.class).list();
         return books;
-    }
-
-    public List<Book> getBooks(int first, int pageSize, String sortField, boolean asc) {
-
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        Criteria criteria = session.createCriteria(Book.class);
-
-        criteria.setFirstResult(first);
-        criteria.setMaxResults(pageSize);
-
-        if(sortField != null) {
-            if(asc) {
-                criteria.addOrder(Order.asc(sortField));
-            }
-            else {
-                criteria.addOrder(Order.desc(sortField));
-            }
-        }
-
-        return criteria.list();
-    }
-
-    public Long getTotalRegistr() {
-
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        Criteria criteria = session.createCriteria(Book.class);
-        criteria.setProjection(Projections.rowCount());
-        return (Long) criteria.uniqueResult();
     }
 }
--- src/main/java/book/project/beans/EditBean.java
+++ src/main/java/book/project/beans/EditBean.java
@@ -2,6 +2,7 @@ package book.project.beans;
 
 import book.project.ListQuery;
 import book.project.model.Book;
+import book.project.model.User;
 import org.primefaces.event.CellEditEvent;
 
 import javax.annotation.PostConstruct;
@@ -10,6 +11,7 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 @ManagedBean
@@ -17,9 +19,25 @@ import java.util.List;
 public class EditBean implements Serializable {
 
     private List<Book> books;
+    private List<User> filteredBookUser;
+    private User user1;
 
     public List<Book> getBooks() {
         return books;
+    }
+
+
+    public List<User> getFioSearch(String query){
+        List<User> filteredFio = new ArrayList<User>();
+
+        for (int i = 0; i < books.size(); i++) {
+            Book book = books.get(i);
+            User us = book.getBookUser();
+            if(us.getFio().toLowerCase().contains(query)) {
+                filteredFio.add(us);
+            }
+        }
+        return filteredFio;
     }
 
     @PostConstruct
@@ -40,5 +58,26 @@ public class EditBean implements Serializable {
             FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Успешно изменено", "Old: " + oldValue + ", New:" + newValue);
             FacesContext.getCurrentInstance().addMessage(null, msg);
         }
+    }
+
+    public User getUser1() {
+        return user1;
+    }
+
+    public void setUser1(User user1) {
+        this.user1 = user1;
+    }
+
+
+    public List<User> getFilteredBookUser() {
+        return filteredBookUser;
+    }
+
+    public void setFilteredBookUser(List<User> filteredBookUser) {
+        this.filteredBookUser = filteredBookUser;
+    }
+
+    public String showData() {
+        return "index.xhtml?faces-redirect=true";
     }
 }
